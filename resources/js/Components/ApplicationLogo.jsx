a5ac89a56cf10ce9652587ead16dd3ee02693916
bilaljan3187 <bilaljan318718@gmail.com>
2024-05-03 08:47:26 +0500

    first commit

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,10 @@
+export default function ApplicationLogo(props) {
+    return (
+
+        <div {...props}  className="flex items-center justify-center">
+        <div className="text-center">
+            <img  src="https://ihp.gkp.pk/wp-content/uploads/2022/05/KP_logo.png" alt="logo" className="w-20 mx-auto" />
+        </div>
+    </div>
+    );
+}
---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,95 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    const handleImageError = () => {
+        document.getElementById('screenshot-container')?.classList.add('!hidden');
+        document.getElementById('docs-card')?.classList.add('!row-span-1');
+        document.getElementById('docs-card-content')?.classList.add('!flex-row');
+        document.getElementById('background')?.classList.add('!hidden');
+    };
+
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+                <img
+                    id="background"
+                    className="absolute -left-20 top-0 max-w-[877px]"
+                    src="https://laravel.com/assets/img/welcome/background.svg"
+                />
+                <div className="relative min-h-screen flex flex-col items-center  selection:bg-[#FF2D20] selection:text-white">
+                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                        <header className="grid items-center grid-cols-2 gap-2 py-10 lg:grid-cols-3">
+                            <div className="flex lg:justify-center lg:col-start-2">
+                            <img  src="https://ihp.gkp.pk/wp-content/uploads/2022/05/KP_logo.png" alt="logo" className="w-20 mx-auto" />
+                            </div>
+                            <nav className="flex justify-end flex-1 -mx-3">
+                                {auth.user ? (
+                                    <Link
+                                        href={route('dashboard')}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </Link>
+                                ) : (
+                                    <>
+                                        <Link
+                                            href={route('login')}
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Log in
+                                        </Link>
+                                        <Link
+                                            href={route('register')}
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Register
+                                        </Link>
+                                    </>
+                                )}
+                            </nav>
+                        </header>
+
+                        <main className="mt-6">
+                            <div className="grid  place-items-center">
+                                <a
+                                    href=""
+                                    id="docs-card"
+                                    className="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
+                                >
+                                    <div
+                                        id="screenshot-container"
+                                        className="relative flex items-stretch flex-1 w-full"
+                                    >
+                                        <img
+                                            src="https://laravel.com/assets/img/welcome/docs-dark.svg"
+                                            alt="Laravel documentation screenshot"
+                                            className="hidden aspect-video h-full w-full flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.25)] dark:block"
+                                        />
+                                        <div className="absolute -bottom-16 -left-16 h-40 w-[calc(100%+8rem)] bg-gradient-to-b from-transparent via-white to-white dark:via-zinc-900 dark:to-zinc-900"></div>
+                                    </div>
+
+                                    <div className="relative flex items-center gap-6 lg:items-end">
+                                        <div id="docs-card-content" className="flex items-start gap-6 lg:flex-col">
+                                            <div className="pt-3 sm:pt-5 lg:pt-0 text-center">
+                                                <h2 className="text-xl font-semibold text-black dark:text-white">
+                                                    INTEGRATED HEALTH PROJECT
+                                                </h2>
+                                                <p className="mt-4 text-sm/relaxed">
+                                                    Integration of Health Services delivery With Special Focus on MNCH, LHW and Nutrition Program
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </main>
+                        <footer className="py-16 text-sm text-center text-black dark:text-white/70">
+                           Developed by <b>Bilal Muhammad</b>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
